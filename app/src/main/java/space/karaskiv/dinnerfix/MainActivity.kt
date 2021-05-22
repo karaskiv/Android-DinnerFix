5f6914d24fb6786d40a6d12fd8d2ddcb7f83b14a
Karaskiv <82354360+Karaskiv@users.noreply.github.com>
2021-05-22 15:22:15 +0700

    test: all done

--- app/src/main/java/space/karaskiv/dinnerfix/MainActivity.kt
+++ app/src/main/java/space/karaskiv/dinnerfix/MainActivity.kt
@@ -2,10 +2,30 @@ package space.karaskiv.dinnerfix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Noodles", "Hamburger", "Pizza", "Steak")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPick.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        btnAdd.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+            println(foodList)
+        }
+
     }
 }
